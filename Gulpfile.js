5e810b0f10558c17793b42ac57ecf379d5cc18e2
Scott Opell <me@scottopell.com>
2019-11-23 10:32:21 -0500

    Updates deps and build pipeline

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,28 +1,27 @@
-var gulp       = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
-var ts         = require('gulp-typescript');
-var babel      = require('gulp-babel');
-var del        = require('del');
+var gulp = require("gulp");
+var sourcemaps = require("gulp-sourcemaps");
+var ts = require("gulp-typescript");
+var babel = require("gulp-babel");
+var del = require("del");
+var notify = require("gulp-notify");
 
+var tsProject = ts.createProject("./tsconfig.json");
 
-var tsProject = ts.createProject('./tsconfig.json');
-
-gulp.task('build', function() {
-  return gulp.src(['src/**/*.ts', 'src/index.ts'])
+gulp.task("build", function() {
+  return gulp
+    .src(["src/**/*.ts"])
     .pipe(sourcemaps.init())
-    .pipe(ts(tsProject))
+    .pipe(tsProject())
     .pipe(babel())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist'));
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("dist"))
+    .pipe(notify("Build Finished"));
 });
 
-gulp.task('watch', function () {
-  gulp.watch('src/**/*.ts', ['build']);
+gulp.task("watch", function() {
+  gulp.watch("src/**/*.ts", ["build"]);
 });
 
-gulp.task('clean', function(){
-  return del([
-    'dist/**/*',
-    'dist/*',
-  ]);
+gulp.task("clean", function() {
+  return del(["dist/**/*", "dist/*"]);
 });
